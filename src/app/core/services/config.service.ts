153d0490e04620bbda65dda99008d541858b3a4a
Sergio Correa <sergiolc@hotmail.com>
2018-07-05 16:42:37 +0100

    Fix on service url

--- src/app/core/services/config.service.ts
+++ src/app/core/services/config.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
 export class ConfigService {
 
     // apiUrl = 'http://localhost:8080';
-    apiUrl = 'https://slc-streaming-service.herokuapp.com/';
+    apiUrl = 'https://slc-streaming-service.herokuapp.com';
 
 }
